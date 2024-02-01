a3ace7dd5cf300cd6c94700d015e4f86f462a643
mr-habe3b <habeebs647@gmail.com>
2024-02-01 16:43:21 +0530

    <message>

---
+++ node1project/index.js
@@ -0,0 +1,18 @@
+import  express from 'express'; //importing express module
+import path from 'path';//path is a core module of nodejs
+
+
+const app = express();//creating the express server
+
+app.set("view engine", "ejs");//setting the view engine to ejs
+app.get("/", (req, res) => {
+    // const pathLocation = path.resolve(); //use to locate the current directory
+    // res.sendfile(path.join(pathLocation, "./index.html"));//use to locate the current directory 
+    // res.sendfile("index.html");//its is common to use this in the production to send the file as per response
+    res.render("index", {name:"Mrhabe3b"});
+});
+
+app.server = app.listen(3000, () => {// 3000 is the port number
+    console.log("server is running on port 3000");//    
+});
+// console.log("welcome to node1project");
